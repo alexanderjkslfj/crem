b0850b439b73e262b03103b34288726bf8cdf676
alexanderjkslfj <117545308+alexanderjkslfj@users.noreply.github.com>
2024-09-15 14:52:51 +0200

    add tests

--- tests/general.rs
+++ tests/general.rs
@@ -3,16 +3,18 @@ mod tests {
     use crem::*;
 
     #[test]
+    fn test_integers() {
+        assert_eq!(Op::from(3), Op::from(3));
+        assert_ne!(Op::from(2), Op::from(4));
+        assert_eq!(Op::from(3).calc(), 3.0);
+    }
+
+    #[test]
     fn test_addition() {
-        {
-            assert_eq!(Op::from(4) + Op::from(3), Op::from(7));
-            assert_eq!(Op::from(0) + Op::from(0), Op::from(0));
-            assert_eq!(
-                Op::from(1) + 2.into() + 3.into() + 4.into(),
-                10.into()
-            );
-            assert_eq!((Op::from(1) + Op::from(2)).calc(), 3.0);
-        }
+        assert_eq!(Op::from(4) + Op::from(3), Op::from(7));
+        assert_eq!(Op::from(0) + Op::from(0), Op::from(0));
+        assert_eq!(Op::from(1) + 2.into() + 3.into() + 4.into(), 10.into());
+        assert_eq!((Op::from(1) + Op::from(2)).calc(), 3.0);
     }
 
     #[test]
@@ -36,19 +38,28 @@ mod tests {
     }
 
     #[test]
+    fn test_multiplication() {
+        assert_eq!(Op::from(2) * Op::from(3), Op::from(6));
+        assert_eq!(Op::from(1) * Op::from(3), Op::from(3));
+        assert_eq!(Op::from(0) * Op::from(3), Op::from(0));
+    }
+
+    #[test]
+    fn test_adding_multiplications() {
+        assert_eq!((Op::from(3) * Op::from(6)) + (Op::from(2) * Op::from(5)), Op::from(28));
+        assert_eq!((Op::from(5) * Op::div(1, 3)) + (Op::from(5) * Op::div(2, 3)), Op::from(5));
+        assert_eq!((Op::from(5) * Op::div(1, 6)) + (Op::from(5) * Op::div(2, 6)), Op::div(5, 2));
+    }
+
+    #[test]
     fn test_adding_divisions() {
-        assert_eq!(
-            Op::div(1, 10) + Op::div(2, 10),
-            Op::div(3, 10)
-        );
+        assert_eq!(Op::div(1, 10) + Op::div(2, 10), Op::div(3, 10));
         // assert_eq!(0.1 + 0.2, 0.3) would panic
         assert_eq!(
             (Op::div(1, 10) + Op::div(2, 10)).calc(),
             Op::div(3, 10).calc()
         );
-        assert_eq!(
-            Op::div(2, 3) + Op::div(1, 6),
-            Op::div(5, 6)
-        );
+        assert_eq!(Op::div(2, 3) + Op::div(1, 6), Op::div(5, 6));
+        assert_eq!(Op::div(1, 3) + Op::div(2, 3), Op::from(1));
     }
 }
