f1398577f42b1a4c73a257f18a524fc7232020a7
alexanderjkslfj <117545308+alexanderjkslfj@users.noreply.github.com>
2024-09-23 15:22:50 +0200

    move minus removal to first parsing step

--- src/parse_string.rs
+++ src/parse_string.rs
@@ -10,10 +10,8 @@ pub enum TryFromStrError {
 }
 
 pub fn parse_string(value: &str) -> Result<Term, TryFromStrError> {
-    // converts "5 + (3*3) * -3" to [Term, '+', Term, '*', '-', Term]
-    let mut flat = parse_to_flat(value)?;
-    // converts [Term, '+', Term, '*', '-', Term] to [Term, '+', Term, '*', Term]
-    remove_minuses(&mut flat);
+    // converts "5 + (3*3) * -3" to [Term, '+', Term, '*', Term]
+    let flat = parse_to_flat(value)?;
     // converts [Term, '+', Term, '*', Term] to Term
     Ok(fold_flat(flat))
 }
@@ -56,47 +54,25 @@ fn fold_flat(input: Vec<Result<Term, char>>) -> Term {
         .fold(Term::from(0u32), |acc, term| acc + term)
 }
 
-fn remove_minuses(input: &mut Vec<Result<Term, char>>) {
-    let mut i = 0;
-    while i < input.len() {
-        let Err(char) = input[i] else {
-            i += 1;
-            continue;
-        };
-        if char == '-' {
-            match &input[i + 1] {
-                Err(_) => {
-                    let _ = input.remove(i + 1);
-                }
-                Ok(_) => {
-                    let term = input.remove(i + 1);
-                    input.insert(i + 1, Ok(-term.unwrap()));
-                }
-            }
-            match &input[i - 1] {
-                Err(_) => {
-                    let _ = input.remove(i);
-                }
-                Ok(_) => {
-                    input[i] = Err('+');
-                }
-            };
-        } else {
-            i += 1;
-        }
-    }
-}
-
 fn parse_to_flat(value: &str) -> Result<Vec<Result<Term, char>>, TryFromStrError> {
     let mut outputs = Vec::new();
 
     enum States {
-        Start,                                                // Start of term; anything goes
-        PostTerm,                    // After a term; expects an operation (or brackets)
-        PostOp,                      // After an operation; expects a term (or a minus)
-        PreComma(String /*buffer*/), // Number before the comma
-        PostComma(u32 /*precomma value*/, String /*buffer*/), // Number after the comma
-        Brackets(usize /*bracket depth*/, String /*buffer*/), // A term inside brackets
+        Start,    // Start of term; anything goes
+        PostTerm, // After a term; expects an operation (or brackets)
+        PostOp,   // After an operation; expects a term (or a minus)
+        PostMinus(Box<States> /*previous state*/),
+        PreComma(String /*buffer*/, bool /*positive number?*/), // Number before the comma
+        PostComma(
+            u32,    /*precomma value*/
+            String, /*buffer*/
+            bool,   /*positive number?*/
+        ), // Number after the comma
+        Brackets(
+            usize,  /*bracket depth*/
+            String, /*buffer*/
+            bool,   /*positive?*/
+        ), // A term inside brackets
     }
 
     let mut state = States::Start;
@@ -104,159 +80,215 @@ fn parse_to_flat(value: &str) -> Result<Vec<Result<Term, char>>, TryFromStrError
         match state {
             States::Start => match char {
                 '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                    state = States::PreComma(String::from(char));
+                    state = States::PreComma(String::from(char), true);
                 }
                 '.' => {
-                    state = States::PostComma(0, String::new());
+                    state = States::PostComma(0, String::new(), true);
                 }
-                '+' | '-' | '*' | '/' => {
+                '+' | '*' | '/' => {
                     outputs.push(Err(char));
                     state = States::PostOp;
                 }
+                '-' => {
+                    state = States::PostMinus(Box::new(States::Start));
+                }
                 '(' => {
-                    state = States::Brackets(1, String::new());
+                    state = States::Brackets(1, String::new(), true);
                 }
                 any if any.is_whitespace() => (),
                 any => return Err(TryFromStrError::UnexpectedCharacter(any)),
             },
             States::PostTerm => match char {
-                '+' | '-' | '*' | '/' => {
+                '+' | '*' | '/' => {
                     outputs.push(Err(char));
                     state = States::PostOp;
                 }
+                '-' => {
+                    state = States::PostMinus(Box::new(States::PostTerm));
+                }
                 '(' => {
                     outputs.push(Err('*'));
-                    state = States::Brackets(1, String::new());
+                    state = States::Brackets(1, String::new(), true);
                 }
                 any if any.is_whitespace() => (),
                 any => return Err(TryFromStrError::UnexpectedCharacter(any)),
             },
             States::PostOp => match char {
                 '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                    state = States::PreComma(String::from(char));
+                    state = States::PreComma(String::from(char), true);
                 }
                 '.' => {
-                    state = States::PostComma(0, String::new());
+                    state = States::PostComma(0, String::new(), true);
                 }
                 '-' => {
-                    outputs.push(Err(char));
+                    state = States::PostMinus(Box::new(States::PostOp));
                 }
                 '(' => {
-                    state = States::Brackets(1, String::new());
+                    state = States::Brackets(1, String::new(), true);
                 }
                 any if any.is_whitespace() => (),
                 any => return Err(TryFromStrError::UnexpectedCharacter(any)),
             },
-            States::PreComma(mut buffer) => match char {
+            States::PreComma(mut buffer, positive) => match char {
                 '.' => {
                     let Ok(parsed_buffer) = buffer.parse::<u32>() else {
                         panic!("Internal library error.");
                     };
-                    state = States::PostComma(parsed_buffer, String::new());
+                    state = States::PostComma(parsed_buffer, String::new(), positive);
                 }
                 '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
                     buffer.push(char);
-                    state = States::PreComma(buffer);
+                    state = States::PreComma(buffer, positive);
                 }
                 '(' => {
                     let Ok(parsed_buffer) = buffer.parse::<u32>() else {
                         panic!("Internal library error.");
                     };
-                    outputs.push(Ok(Term::from(parsed_buffer)));
+                    let term = Term::from(parsed_buffer);
+                    outputs.push(Ok(if positive { term } else { -term }));
                     outputs.push(Err('*'));
-                    state = States::Brackets(1, String::new());
+                    state = States::Brackets(1, String::new(), true);
                 }
                 '+' | '-' | '*' | '/' => {
                     let Ok(parsed_buffer) = buffer.parse::<u32>() else {
                         panic!("Internal library error.");
                     };
-                    outputs.push(Ok(Term::from(parsed_buffer)));
-                    outputs.push(Err(char));
-                    state = States::PostOp;
+                    let term = Term::from(parsed_buffer);
+                    outputs.push(Ok(if positive { term } else { -term }));
+                    if char == '-' {
+                        state = States::PostMinus(Box::new(States::PostTerm));
+                    } else {
+                        outputs.push(Err(char));
+                        state = States::PostOp;
+                    }
                 }
                 any if any.is_whitespace() => {
                     let Ok(parsed_buffer) = buffer.parse::<u32>() else {
                         panic!("Internal library error.");
                     };
-                    outputs.push(Ok(Term::from(parsed_buffer)));
+                    let term = Term::from(parsed_buffer);
+                    outputs.push(Ok(if positive { term } else { -term }));
                     state = States::PostTerm;
                 }
                 any => return Err(TryFromStrError::UnexpectedCharacter(any)),
             },
-            States::PostComma(pre, mut buffer) => {
-                match char {
-                    '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                        buffer.push(char);
-                        state = States::PostComma(pre, buffer);
-                    }
-                    '(' => {
-                        let Ok(parsed_buffer) = buffer.parse::<u32>() else {
-                            panic!("Internal library error.");
-                        };
-                        outputs.push(Ok(Term::from(pre)
-                            + Term::div(parsed_buffer, 10u32.pow(buffer.len() as u32))));
-                        outputs.push(Err('*'));
-                        state = States::Brackets(1, String::new());
-                    }
-                    '+' | '-' | '*' | '/' => {
-                        let Ok(parsed_buffer) = buffer.parse::<u32>() else {
-                            panic!("Internal library error.");
-                        };
-                        outputs.push(Ok(Term::from(pre)
-                            + Term::div(parsed_buffer, 10u32.pow(buffer.len() as u32))));
+            States::PostComma(pre, mut buffer, positive) => match char {
+                '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                    buffer.push(char);
+                    state = States::PostComma(pre, buffer, positive);
+                }
+                '(' => {
+                    let Ok(parsed_buffer) = buffer.parse::<u32>() else {
+                        panic!("Internal library error.");
+                    };
+                    let term =
+                        Term::from(pre) + Term::div(parsed_buffer, 10u32.pow(buffer.len() as u32));
+                    outputs.push(Ok(if positive { term } else { -term }));
+                    outputs.push(Err('*'));
+                    state = States::Brackets(1, String::new(), true);
+                }
+                '+' | '-' | '*' | '/' => {
+                    let Ok(parsed_buffer) = buffer.parse::<u32>() else {
+                        panic!("Internal library error.");
+                    };
+                    let term =
+                        Term::from(pre) + Term::div(parsed_buffer, 10u32.pow(buffer.len() as u32));
+                    outputs.push(Ok(if positive { term } else { -term }));
+                    if char == '-' {
+                        state = States::PostMinus(Box::new(States::PostTerm));
+                    } else {
                         outputs.push(Err(char));
                         state = States::PostOp;
                     }
-                    any if any.is_whitespace() => {
-                        let Ok(parsed_buffer) = buffer.parse::<u32>() else {
-                            panic!("Internal library error.");
-                        };
-                        outputs.push(Ok(Term::from(pre)
-                            + Term::div(parsed_buffer, 10u32.pow(buffer.len() as u32))));
-                        state = States::PostTerm;
-                    }
-                    any => return Err(TryFromStrError::UnexpectedCharacter(any)),
                 }
-            }
-            States::Brackets(depth, mut buffer) => match char {
+                any if any.is_whitespace() => {
+                    let Ok(parsed_buffer) = buffer.parse::<u32>() else {
+                        panic!("Internal library error.");
+                    };
+                    let term =
+                        Term::from(pre) + Term::div(parsed_buffer, 10u32.pow(buffer.len() as u32));
+                    outputs.push(Ok(if positive { term } else { -term }));
+                    state = States::PostTerm;
+                }
+                any => return Err(TryFromStrError::UnexpectedCharacter(any)),
+            },
+            States::Brackets(depth, mut buffer, positive) => match char {
                 '(' => {
                     buffer.push('(');
-                    state = States::Brackets(depth + 1, buffer);
+                    state = States::Brackets(depth + 1, buffer, positive);
                 }
                 ')' => {
                     if depth == 1 {
-                        outputs.push(Ok(parse_string(buffer.as_str())?));
+                        let term = parse_string(buffer.as_str())?;
+                        outputs.push(Ok(if positive { term } else { -term }));
                         state = States::PostTerm;
                     } else {
                         buffer.push(')');
-                        state = States::Brackets(depth - 1, buffer);
+                        state = States::Brackets(depth - 1, buffer, positive);
                     }
                 }
                 any => {
                     buffer.push(any);
-                    state = States::Brackets(depth, buffer);
+                    state = States::Brackets(depth, buffer, positive);
                 }
+            },
+            States::PostMinus(ref prev_state) => match char {
+                '-' => {
+                    match prev_state.as_ref() {
+                        States::Start => {
+                            state = States::Start;
+                        }
+                        States::PostOp => {
+                            state = States::PostOp;
+                        }
+                        States::PostTerm => {
+                            outputs.push(Err('+'));
+                            state = States::PostOp;
+                        }
+                        _ => panic!("Internal library error."),
+                    };
+                }
+                '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                    if matches!(prev_state.as_ref(), States::PostTerm) {
+                        outputs.push(Err('+'));
+                    }
+                    state = States::PreComma(String::from(char), false);
+                }
+                '.' => {
+                    if matches!(prev_state.as_ref(), States::PostTerm) {
+                        outputs.push(Err('+'));
+                    }
+                    state = States::PostComma(0, String::new(), false);
+                }
+                '(' => {
+                    if matches!(prev_state.as_ref(), States::PostTerm) {
+                        outputs.push(Err('+'));
+                    }
+                    state = States::Brackets(1, String::new(), false);
+                }
+                any if any.is_whitespace() => (),
+                any => return Err(TryFromStrError::UnexpectedCharacter(any)),
             },
         }
     }
 
     match state {
-        States::Brackets(_, _) | States::PostOp | States::Start => {
+        States::Brackets(_, _, _) | States::PostMinus(_) | States::PostOp | States::Start => {
             return Err(TryFromStrError::UnexpectedEof)
         }
-        States::PostComma(pre, buffer) => {
+        States::PostComma(pre, buffer, positive) => {
             let Ok(parsed_buffer) = buffer.parse::<u32>() else {
                 panic!("Internal library error.");
             };
-            outputs.push(Ok(
-                Term::from(pre) + Term::div(parsed_buffer, 10u32.pow(buffer.len() as u32))
-            ));
+            let term = Term::from(pre) + Term::div(parsed_buffer, 10u32.pow(buffer.len() as u32));
+            outputs.push(Ok(if positive { term } else { -term }));
         }
-        States::PreComma(buffer) => {
+        States::PreComma(buffer, positive) => {
             let Ok(parsed_buffer) = buffer.parse::<u32>() else {
                 panic!("Internal library error.");
             };
-            outputs.push(Ok(Term::from(parsed_buffer)));
+            let term = Term::from(parsed_buffer);
+            outputs.push(Ok(if positive { term } else { -term }));
         }
         States::PostTerm => (),
     };
